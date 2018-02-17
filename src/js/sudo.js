c5fae732f0cb3ba34da50b10e2fe00c0a26b3d69
Unknown <jackysoon871008@gmail.com>
2018-02-18 00:46:33 +0800

    Show the answer

--- src/js/sudo.js
+++ src/js/sudo.js
@@ -13,6 +13,23 @@ $('document').ready(function(){
                 }
             }
         }
-        console.log(solver.next);
+        for(var i = 0; i < solver.next.length; i++){
+            var alert_num = '.raw_' + solver.next[i].row + ' .colum_' + solver.next[i].col
+            document.querySelector(alert_num).style.backgroundColor = "#EC8E26";
+            document.querySelector(alert_num).name = solver.next[i].num;
+        }
     })
+    $('.colum').click(function(){
+        if(1 <= this.name && this.name <= 9){
+            this.value = this.name;
+            this.style.background = "transparent";
+            solver.set(parseInt(this.dataset.raw, 10), parseInt(this.dataset.colum, 10), parseInt(this.value, 10));
+            for(var i = 0; i < solver.next.length; i++){
+                var alert_num = '.raw_' + solver.next[i].row + ' .colum_' + solver.next[i].col
+                document.querySelector(alert_num).style.backgroundColor = "#EC8E26";
+                document.querySelector(alert_num).name = solver.next[i].num;
+            }
+        }
+    })
+
 })
