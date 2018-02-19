716864ecf5289741d323b9a701713bc3b5e2f022
kusiro <jackysoon871008@gmail.com>
2018-02-19 23:20:34 +0800

    Add reason_cause

--- src/js/sudo.js
+++ src/js/sudo.js
@@ -17,18 +17,45 @@ $('document').ready(function(){
             var alert_num = '.raw_' + solver.next[i].row + ' .colum_' + solver.next[i].col
             document.querySelector(alert_num).style.backgroundColor = "#EC8E26";
             document.querySelector(alert_num).name = solver.next[i].num;
+            document.querySelector(alert_num).dataset.key = i;
         }
     })
+    $('.colum').hover(function(){
+        if(1 <= this.name && this.name <= 9){
+            var reason_cause = solver.next[this.dataset.key].reason;
+            for(var i = 0; i < reason_cause.length; i++){
+                var reason_num = '.raw_' + reason_cause[i][0] + ' .colum_' + reason_cause[i][1];
+                document.querySelector(reason_num).style.backgroundColor = "green";
+            }
+        }
+    });
+    $('.colum').mouseleave(function(){
+        if(1 <= this.name && this.name <= 9){
+            var reason_cause = solver.next[this.dataset.key].reason;
+            for(var i = 0; i < reason_cause.length; i++){
+                var reason_num = '.raw_' + reason_cause[i][0] + ' .colum_' + reason_cause[i][1];
+                document.querySelector(reason_num).style.background = "transparent";
+            }
+        }
+    });
+
     $('.colum').click(function(){
         if(1 <= this.name && this.name <= 9){
             this.value = this.name;
             this.style.background = "transparent";
+            var reason_cause = solver.next[this.dataset.key].reason;
+            for(var i = 0; i < reason_cause.length; i++){
+                var reason_num = '.raw_' + reason_cause[i][0] + ' .colum_' + reason_cause[i][1];
+                document.querySelector(reason_num).style.background = "transparent";
+            }
+            
             solver.set(parseInt(this.dataset.raw, 10), parseInt(this.dataset.colum, 10), parseInt(this.value, 10));
             for(var i = 0; i < solver.next.length; i++){
                 var alert_num = '.raw_' + solver.next[i].row + ' .colum_' + solver.next[i].col
                 document.querySelector(alert_num).style.backgroundColor = "#EC8E26";
                 document.querySelector(alert_num).name = solver.next[i].num;
             }
+
         }
     })
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import style from './style/style.scss';
 import html from '../index.html';
 import Sudo from './js/sudo.js';
 import Vue from 'vue';
+import Fancy_ad from './js/fancy_admin.js';
 
 var vm = new Vue({
     el: ".containor"
