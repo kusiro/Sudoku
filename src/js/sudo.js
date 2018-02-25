123f66a1c1cd39ac84d2aded6ec83a3e44931510
kusiro <jackysoon871008@gmail.com>
2018-02-25 18:11:42 +0800

    Add buttom select feature

--- src/js/sudo.js
+++ src/js/sudo.js
@@ -1,8 +1,44 @@
 import Solver from './sudoku_solver.js';
 
 var solver = new Solver;
+var focusPoint = {};
+
 
 $('document').ready(function(){
+//---------------------上下左右--------------------
+
+    $('input').focus(function(){
+        focusPoint.raw = parseInt(this.dataset.raw, 10);
+        focusPoint.colum = parseInt(this.dataset.colum, 10);
+    })
+    $('input').keydown(function(event){
+        var keyPress = event.originalEvent.key;
+        if(keyPress === "ArrowUp"){
+            focusPoint.raw -= 1;
+            if(focusPoint.raw < 0){
+                focusPoint.raw = 8;
+            }
+        }else if(keyPress === "ArrowDown"){
+            focusPoint.raw += 1;
+            if(focusPoint.raw > 8){
+                focusPoint.raw = 0;
+            }
+        }else if(keyPress === "ArrowLeft"){
+            focusPoint.colum -= 1;
+            if(focusPoint.colum < 0){
+                focusPoint.colum = 8;
+            }
+        }else if(keyPress === "ArrowRight"){
+            focusPoint.colum += 1;
+            if(focusPoint.colum > 8){
+                focusPoint.colum = 0;
+            }
+        }else{}
+        $(".raw_" + focusPoint.raw + " .colum_" + focusPoint.colum).focus();
+    })
+
+//---------------------Let`s 按鈕--------------------
+
     $('.button').click(function(){
         for(var i = 0; i < 9; i++){
             for(var j = 0; j < 9; j++){
@@ -21,6 +57,7 @@ $('document').ready(function(){
         }
     })
 
+//---------------------Display reason--------------------
 
     $('.colum').hover(function(){
         if(this.dataset.key !== "unset" && this.dataset.key !== "setted"){
@@ -32,8 +69,6 @@ $('document').ready(function(){
             }
         }
     });
-
-
     $('.colum').mouseleave(function(){
         if(this.dataset.key !== "unset" && this.dataset.key !== "setted"){
             var reason_cause = solver.next[this.dataset.key].reason;
@@ -49,14 +84,13 @@ $('document').ready(function(){
         }
     });
 
+//---------------------按下答案(黃色按鈕)--------------------
+
     $('.colum').click(function(){
         if(1 <= this.dataset.val && this.dataset.val <= 9){
-            console.log(solver.next)
             this.value = this.dataset.val;
             this.style.background = "rgb(255, 111, 111)";
             var reason_cause = solver.next[this.dataset.key].reason;
-
-//`````````````````````Display reason````````````````````````````````````````````````
 
             for(var i = 0; i < reason_cause.length; i++){
                 var reason_num = document.querySelector('.raw_' + reason_cause[i][0] + ' .colum_' + reason_cause[i][1]);
@@ -68,7 +102,6 @@ $('document').ready(function(){
                 }
             }
 
-//`````````````````````````````````````````````````````````````````````````````
             solver.set(parseInt(this.dataset.raw, 10), parseInt(this.dataset.colum, 10), parseInt(this.value, 10));
             for(var i = 0; i < solver.next.length; i++){
                 var alert_num = document.querySelector('.raw_' + solver.next[i].row + ' .colum_' + solver.next[i].col);
@@ -78,10 +111,8 @@ $('document').ready(function(){
                     alert_num.dataset.key = i;
                 }
             }
-            console.log(solver.next)
             this.dataset.key = "setted";
             this.dataset.val = "setted";
-            console.log("no")
         }
     })
 
